1f64ac25325d66f1061eb31800217e31203260ec
Aidan Maskelyne <aidan@muttleyville.org>
2024-01-13 10:09:08 +0000

    Add README.md & update treesitter.lua

---
+++ README.md
@@ -0,0 +1,5 @@
+# init.lua
+
+My personal Neovim config.
+
+Run `git clone https://github.com/AidanMaskelyne/init.lua ~/.config/nvim`, and make sure `ripgrep` is installed on your system.
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,3 +1,11 @@
+require("nvim-treesitter.parsers").get_parser_configs().asm = {
+	install_info = {
+		url = "https://github.com/rush-rs/tree-sitter-asm.git",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+}
+
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
 	ensure_installed = { "javascript", "typescript", "rust", "c", "lua", "vim", "vimdoc", "query" },
